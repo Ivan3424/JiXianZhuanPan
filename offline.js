2966341b97975ec3a9a58ecedf9c9203b5461928
Ivan3424 <wwj3424v@163.com>
2018-07-06 16:12:17 +0800

    first

---
+++ offline.js
@@ -0,0 +1,14 @@
+﻿{
+	"version": 1530864712,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/tiledbackground.png",
+		"images/bar-sheet0.png",
+		"images/tiledbackground3.png",
+		"images/tiledbackground4.png",
+		"images/tiledbackground5.png",
+		"images/sprite-sheet0.png"
+	]
+}
